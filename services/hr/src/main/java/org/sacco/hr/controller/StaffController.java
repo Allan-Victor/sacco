76c4a6fab28d696e15a37a03f0905d54d3ea5fa1
kato <samuelkato74@gmail.com>
2025-01-23 14:00:21 +0300

    modified the @Component Bean and also Method 4: Changed @GetMapping to @GetMapping("/next-of-kin") and used a query parameter (@RequestParam) for staffId.     Ensured all methods have unique mappings:

--- services/hr/src/main/java/org/sacco/hr/controller/StaffController.java
+++ services/hr/src/main/java/org/sacco/hr/controller/StaffController.java
@@ -16,24 +16,29 @@ import java.util.List;
 public class StaffController {
 
     public final StaffService staffService;
-
+    // 1. Get staff list ordered by department
     @GetMapping()
     public ResponseEntity<List<StaffDepartmentOrderDTO>> getStaffListOrderedByDepartment(){
         return ResponseEntity.ok(staffService.fetchStaffListOrderedByDepartment());
     }
 
+
+    // 2. Get employee reports by department ID
     @GetMapping("/departments/{departmentId}/employee-reports")
     public ResponseEntity<List<StaffResponseDTO>> getEmployeeReportsByDepartment(@PathVariable("departmentId") Integer departmentId){
         return ResponseEntity.ok(staffService.fetchStaffDepartmentList(departmentId));
     }
 
+    // 3. Get employee reports by job title ID
     @GetMapping("/position/{jobTitleId}/employee-reports")
     public ResponseEntity<List<StaffResponseDTO>> getEmployeeReportsByJobTitle(@PathVariable("jobTitleId") Integer jobTitleId){
         return ResponseEntity.ok(staffService.fetchStaffPositionList(jobTitleId));
     }
 
-    @GetMapping
-    public ResponseEntity<List<StaffNextOfKinDTO>> getNextOfKinByStaffId(Integer staffId){
+    // 4. Get next of kin by staff ID (use query parameter)
+    //URL: GET /api/v1/staff/next-of-kin?staffId={staffId}
+    @GetMapping("/next-of-kin")
+    public ResponseEntity<List<StaffNextOfKinDTO>> getNextOfKinByStaffId(@RequestParam("staffId") Integer staffId){
         return ResponseEntity.ok(staffService.fetchStaffNextOfKin(staffId));
     }
 }
--- services/hr/src/main/java/org/sacco/hr/dto/NextOfKinMapper.java
+++ services/hr/src/main/java/org/sacco/hr/dto/NextOfKinMapper.java
@@ -2,10 +2,12 @@ package org.sacco.hr.dto;
 
 import org.sacco.hr.model.NextOfKin;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class NextOfKinMapper implements RowMapper<NextOfKin> {
     @Override
     public NextOfKin mapRow(ResultSet rs, int rowNum) throws SQLException {
